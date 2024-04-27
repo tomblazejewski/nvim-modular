482ea16081c7ccfaa10aaa494bf14d9326cf3e29
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-04-27 08:02:19 +0100

    Add pyright and hook pyright to the active venv

---
+++ lua/kickstart/plugins/pyright.lua
@@ -0,0 +1,17 @@
+local lspconfig = require 'lspconfig'
+local util = lspconfig.util
+local path = util.path
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'Scripts', 'python')
+  end
+  return path.join(vim.env.HOME, 'venvs', 'venv', 'Scripts', 'python')
+end
+
+lspconfig.pyright.setup {
+  -- ...
+  before_init = function(_, config)
+    config.settings.python.pythonPath = get_python_path(config.root_dir)
+  end,
+}
