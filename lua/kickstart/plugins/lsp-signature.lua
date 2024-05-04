0c281ab5cf24c7b4577d8811564382e452413ced
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-05-04 09:07:51 +0100

    Trying to fix lsp signature

--- lua/kickstart/plugins/lsp-signature.lua
+++ lua/kickstart/plugins/lsp-signature.lua
@@ -4,14 +4,7 @@ return {
   opts = {},
   config = function(_, opts)
     require('lsp_signature').setup(opts)
-    vim.keymap.set({ 'n' }, '<C-k>', function()
-      require('lsp_signature').toggle_float_win()
-    end, {
-      silent = true,
-      noremap = true,
-      desc = 'Toggle signature',
-    })
-    vim.keymap.set({ 'n' }, '<Leader>k', function()
+    vim.keymap.set({ 'n', 'i' }, '<C-s>', function()
       vim.lsp.buf.signature_help()
     end, { silent = true, noremap = true, desc = 'toggle signature' })
   end,
