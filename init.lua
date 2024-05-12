d5c306a229875d2d8a79bad88ed42fcc4a7bfba7
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-05-12 12:57:18 +0100

    Remove notify

--- init.lua
+++ init.lua
@@ -93,7 +93,6 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-
 -- [[ Setting options ]]
 require 'options'
 
@@ -106,6 +105,6 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
-vim.notify = require 'notify'
+-- vim.notify = require 'notify'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
