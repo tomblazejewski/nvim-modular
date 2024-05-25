f628754b3bc3ae10e483846750478203e871de43
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-05-25 14:38:24 +0100

    Add hard time

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -71,7 +71,7 @@ require('lazy').setup({
   require 'kickstart.plugins.oil',
   -- require 'kickstart.plugins.notify',
   require 'kickstart.plugins.diffview',
-
+  require 'kickstart.plugins.hardtime',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
