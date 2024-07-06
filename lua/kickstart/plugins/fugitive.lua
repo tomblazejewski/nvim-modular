063da42909f10f2c147f6b45714d1835385c6708
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-07-06 22:23:57 +0100

    Allow to blame without virtual text

--- lua/kickstart/plugins/fugitive.lua
+++ lua/kickstart/plugins/fugitive.lua
@@ -1,3 +1,13 @@
 return {
   'tpope/vim-fugitive',
+  config = function()
+    vim.keymap.set('n', '<leader>gbf', function()
+      vim.cmd 'LspLensOff'
+      vim.cmd 'Git blame'
+    end, { desc = '[G]it [B]lame [F]ile' })
+    vim.keymap.set('n', '<leader>gbe', function()
+      vim.cmd 'q'
+      vim.cmd 'LspLensOn'
+    end, { desc = '[G]it [B]lame [E]xit' })
+  end,
 }
