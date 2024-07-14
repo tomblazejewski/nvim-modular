546a501872ebf2138cc09cbe03b126bc1a97d128
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-07-14 15:29:45 +0100

    Add more diffviewoptions

--- lua/kickstart/plugins/diffview.lua
+++ lua/kickstart/plugins/diffview.lua
@@ -2,5 +2,7 @@ return {
   'sindrets/diffview.nvim',
   config = function()
     vim.keymap.set('n', '<leader>gdf', ':DiffviewFileHistory % <cr>', { desc = '[G]it [D]iff [F]ile' })
+    vim.keymap.set('n', '<leader>gdo', ':DiffviewOpen<cr>', { desc = '[G]it [D]iffView [O]pen' })
+    vim.keymap.set('n', '<leader>gdc', ':DiffviewClose<cr>', { desc = '[G]it [D]iffView [C]lose' })
   end,
 }
