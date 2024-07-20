6e7ea8cc83d5f7cfa34536d9036797cd86c39b47
tomblazejewski <tom.blazejewski@auroraer.com>
2024-07-20 13:48:43 +0100

    Remove key bindings in command mode

--- lua/kickstart/plugins/flash.lua
+++ lua/kickstart/plugins/flash.lua
@@ -3,12 +3,17 @@ return {
   event = 'VeryLazy',
   ---@type Flash.Config
   opts = {},
+  modes = {
+    char = {
+      jump_labels = true,
+      enabled = true,
+    },
+  },
   -- stylua: ignore
   keys = {
-{ "<leader>jj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
- { "<leader>jt", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+{ "<leader>jj", mode = { "n",  "o" }, function() require("flash").jump() end, desc = "Flash" },
+ { "<leader>jt", mode = { "n",  "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
  { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
  { "<leader>js", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
- { "<leader>jf", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
  },
 }
