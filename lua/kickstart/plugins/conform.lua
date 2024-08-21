8001170cd2580149e22d7bfaeff6f1b0ed0ae38e
tomblazejewski <tom.blazejewski@auroraer.com>
2024-08-21 09:04:23 +0100

    Add mypy and trim whitespaces

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -29,7 +29,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'isort', 'black' },
+        python = { 'isort', 'black', 'trim_whitespace' },
         json = { 'jq' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
